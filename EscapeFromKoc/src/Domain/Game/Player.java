824ea174a06e64e29e33cffed313052d44099a78
begyalcin <59124760+begyalcin@users.noreply.github.com>
2022-11-20 23:19:56 +0300

    Created Player.java

---
+++ EscapeFromKoc/src/Domain/Game/Player.java
@@ -0,0 +1,57 @@
+package Domain.Game;
+
+public class Player {
+	public Avatar avatar;
+	public Inventory inventory;
+	double score=0;
+	double health=100;
+	EscapeFromKocGame escapeFromKocGame;	
+	
+	public Player() {
+		inventory = new Inventory();
+		avatar = new Avatar(0);   //unitlength
+		escapeFromKocGame = EscapeFromKocGame.getInstance();		
+	}
+
+	public Avatar getAvatar() {
+		return avatar;
+	}
+
+	public double getScore() {
+		return score;
+	}
+
+	public void setScore(double score) {
+		this.score = score;
+	}
+
+	public double getHealth() {
+		return health;
+	}
+
+	public void setHealth(double health) {
+		this.health = health;
+	}
+
+	public void moveAvatar(String direction) {
+		avatar.move(direction);
+	}
+
+	public void catchPowerup() {
+		Powerup activatedPowerup = avatar.activatePowerup();
+		if(activatedPowerup!=null) {
+			//inventory.increaseNumberOfPowerups();
+		}
+	}
+	public void pickKey() {
+		Key key = avatar.pickKey();		
+	}
+
+	public void pickPowerup(String type) {
+	}
+
+	public void incrementScore(double increment) {
+		score+=increment;
+	}
+
+}
