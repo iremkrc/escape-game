f4aa3d2d4e9a65366de7255a3da01b8e4585615b
meterdogan07 <merdogan18@ku.edu.tr>
2022-12-07 22:44:05 +0300

    Building Object is created. BuildingLayoutPanel is created to host the building mode activity. Also the location class is changed accordingly the grid style.

---
+++ EscapeFromKoc/src/UI/BuildingLayoutPanel.java
@@ -0,0 +1,50 @@
+package UI;
+
+import javax.swing.*;
+
+import Domain.Player.Avatar;
+import Domain.Game.GameKeyListener;
+import Domain.GameObjects.GameObject;
+import Domain.Controllers.GameController;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.LinkedList;
+
+public class BuildingLayoutPanel extends JPanel {
+    GameController game;
+	public BuildingLayoutPanel(String title) {
+        super();
+        game = GameController.getInstance();
+        setSize(1000, 800);
+        setVisible(true);
+		GameKeyListener listeners = new GameKeyListener(game);
+		addMouseListener(listeners);
+    }
+
+    public void repaint( Graphics g ) {  
+    	LinkedList<GameObject> objectList = game.currentBuilding.getObjectList();
+	    for(int i=0; i<objectList.size(); i++) { //Hardcoded
+	    	GameObject obj = objectList.get(i);
+	    	obj.draw(g);
+	    }
+    } 
+    
+    public void paint( Graphics g ) {  
+	    for (int x = 50; x <= 500; x += 50 ){
+	        for (int y = 50; y <= 500; y += 50 ){
+	            g.drawRect(x, y, 50, 50);
+	        }
+	    }
+    	LinkedList<GameObject> objectList = game.currentBuilding.getObjectList();
+	    for(int i=0; i<objectList.size(); i++) { //Hardcoded
+	    	GameObject obj = objectList.get(i);
+	    	obj.draw(g);
+	    }
+    }
+    
+    public Dimension getPreferredSize() {
+        return new Dimension(900,600);
+    }
+}
