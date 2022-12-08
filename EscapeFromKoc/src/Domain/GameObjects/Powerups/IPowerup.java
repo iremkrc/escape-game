7890632be5cd8837ac224aade5a503b11fe3aeda
begyalcin <59124760+begyalcin@users.noreply.github.com>
2022-12-08 23:22:07 +0300

    Updated IPowerup.java

--- EscapeFromKoc/src/Domain/GameObjects/Powerups/IPowerup.java
+++ EscapeFromKoc/src/Domain/GameObjects/Powerups/IPowerup.java
@@ -1,7 +1,13 @@
 package Domain.GameObjects.Powerups;
 
+import Domain.Game.Location;
 
 public interface IPowerup {
 	public String getType();
 	public void activatePowerup();
+	public int getWidth();
+	public int getHeight();
+	public Location getLocation();
+	public void setLocation(double x , double y, int width, int height);
+	public void move();
 }
