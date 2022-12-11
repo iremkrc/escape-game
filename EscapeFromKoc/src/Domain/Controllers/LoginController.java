50bdb403b9c1ef4debfef77e407ca8e9211fe16b
birkancelik18 <birkancelik18@ku.edu.tr>
2022-12-11 17:02:13 +0300

    apply observer pattern to login page

---
+++ EscapeFromKoc/src/Domain/Controllers/LoginController.java
@@ -0,0 +1,69 @@
+package Domain.Controllers;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class LoginController {
+    GameController gameController;
+    private static List<String> userNameList;
+    
+    public LoginController(GameController gameController) {
+        this.gameController = gameController;
+    }
+    
+
+    public List<String> getUsernameList() {
+        userNameList = new ArrayList<String>();
+		Scanner scanner;
+		try {
+			scanner = new Scanner(new File("usernames.txt"));
+			while (scanner.hasNext()) {
+				userNameList.add(scanner.nextLine());
+			}
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+        return userNameList;
+    }
+    public boolean isRegistered(String userName) {
+        if(getUsernameList().contains(userName)){
+            return true;
+        }
+        return false;
+    }
+    public void addUsername(String username) {
+        userNameList.add(username);
+    }
+
+
+    public int signUpUser(String signUpName) {
+        try {
+					
+            FileWriter writer = new FileWriter("usernames.txt", true);
+            
+            if(signUpName.equals("")) {
+                writer.close();
+                return 0;
+            }else if (getUsernameList().contains(signUpName)) {
+                writer.close();
+                return 1;
+            }else{
+                writer.write(signUpName + "\n");
+                getUsernameList().add(signUpName);
+                writer.close();
+                return 2;
+            }
+            
+          } catch (IOException k) {
+            System.out.println("An error occurred.");
+            k.printStackTrace();
+          }
+        return 3;
+    }
+}
