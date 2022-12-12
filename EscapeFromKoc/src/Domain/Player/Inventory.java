8b1aa95ede24466f76effcaa555a9040f5570716
begyalcin <59124760+begyalcin@users.noreply.github.com>
2022-12-13 00:21:28 +0300

    Created Inventory.java

--- EscapeFromKoc/src/Domain/Player/Inventory.java
+++ EscapeFromKoc/src/Domain/Player/Inventory.java
@@ -1,7 +1,57 @@
 package Domain.Player;
 
+import java.util.HashMap;
+
 public class Inventory {
+	public HashMap<String, Integer> powerupsMap = new HashMap<String, Integer>();
 
+	public Inventory() {
+		powerupsMap.put("hint", 0);
+		powerupsMap.put("vest", 0);
+		powerupsMap.put("bottle", 0);
+	}
+
+	public String getPowerup(String powerupType) {
+		boolean powerupFound = checkInventory(powerupType);
+		if(powerupFound){
+            return powerupType;
+        }
+        return null;
+	}
+
+    public int getPowerupCount(String powerupType) {
+		boolean powerupFound = checkInventory(powerupType);
+		if(powerupFound){
+            return powerupsMap.get(powerupType);
+        }
+        return 0;
+	}
+
+    public boolean checkInventory(String item) {
+		if(!powerupsMap.containsKey(item)){
+            return false;
+        }else if(powerupsMap.get(item)==0){
+            return false;
+        }else{
+            return true;
+        } 		
+	}
+
+    public void incrementPowerups(String powerupType) throws Exception {
    
+        if(!powerupsMap.containsKey(powerupType)){
+            throw new Exception("Couldn't find powerup type");
+        }
+        int totalPowerups = powerupsMap.get(powerupType);
+        powerupsMap.put(powerupType,totalPowerups++);
+    }
 
+    public void decrementPowerups(String powerupType) throws Exception {
+   
+        if(!powerupsMap.containsKey(powerupType)){
+            throw new Exception("Couldn't find powerup type.");
+        }
+        int totalPowerups = powerupsMap.get(powerupType);
+        powerupsMap.put(powerupType,totalPowerups--);
+    }
 }
