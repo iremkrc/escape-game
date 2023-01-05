aa607bba6e345f124233c89fcb2cbeb25e9e4657
begyalcin <59124760+begyalcin@users.noreply.github.com>
2023-01-05 23:38:10 +0300

    Updated Catch Powerup Test

--- EscapeFromKoc/src/test/PowerupTest.java
+++ EscapeFromKoc/src/test/PowerupTest.java
@@ -17,188 +17,149 @@ public class PowerupTest {
     private PlayerController player = new PlayerController();
     private PowerupController powerupController = new PowerupController();
     Location location;
-    IPowerup timePowerup;
-    IPowerup bottlePowerup;
-    IPowerup hintPowerup;
-    IPowerup lifePowerup;
-    IPowerup vestPowerup;
-
-  
+    
     @Test
-    public void checkHealthPowerupTest(){
+    public void catchTimePowerupTest() throws Exception{
     	
-    	// Requires: life power-up is collected
+    	// Requires: player wants to catch time power-up 
+    	// Modifies: game time
+    	// Effects: increments game time by 5 
+    	
+    	IPowerup timePowerup = PowerupFactory.getInstance().createPowerup("time");
+    	timePowerup.setLocation(200,100,25,25);
+    	
+    	escapeFromKocGame.setPowerupController(powerupController);
+    	escapeFromKocGame.setPlayer(player); 
+    	powerupController.setPowerup(timePowerup);
+    	
+    	int initTime = escapeFromKocGame.getGameState().getTime();
+    	escapeFromKocGame.catchPowerUp(200, 100);
+    	int afterTime = escapeFromKocGame.getGameState().getTime();
+
+        assertEquals(afterTime, initTime+5);
+        
+    }
+
+
+    @Test
+    public void catchLifePowerupTest() throws Exception{
+
+    	// Requires: player wants to catch life power-up 
     	// Modifies: player's health
     	// Effects: changes the health of the player, increments it 
+    	
+    	IPowerup lifePowerup = PowerupFactory.getInstance().createPowerup("life");
+    	lifePowerup.setLocation(200,100,25,25);
+    	
+    	escapeFromKocGame.setPowerupController(powerupController); 
+    	escapeFromKocGame.setPlayer(player); 
+    	powerupController.setPowerup(lifePowerup);  
+    	
+    	int initHealth = player.getPlayerState().getHealth();
+    	escapeFromKocGame.catchPowerUp(200, 100);
+ 
+        assertEquals(player.getPlayerState().getHealth(), initHealth+1);
+    }
+   
+   
+    @Test
+    public void checkDeletePowerupTest() throws Exception{
 
-        int initHealth = player.getPlayerState().getHealth();
-        player.getPlayerState().setHealth(initHealth+1);
+    	// Requires: player wants to catch vest power-up 
+    	// Modifies: sets power-up to null
+    	// Effects: the collected power-up is now set to null, it is deleted
+    	
+    	IPowerup vestPowerup = PowerupFactory.getInstance().createPowerup("vest");
+    	vestPowerup.setLocation(200,100,25,25);
+    	
+    	escapeFromKocGame.setPowerupController(powerupController); 
+    	escapeFromKocGame.setPlayer(player); 
+    	powerupController.setPowerup(vestPowerup);  
+    
+    	escapeFromKocGame.catchPowerUp(200, 100);
+ 
+        assertEquals(powerupController.getPowerup(), null);
 
-        assertNotEquals(player.getPlayerState().getHealth(), initHealth);
     }
 
     @Test
-    public void checkTimePowerupTest(){
-    	
-    	// Requires: time power-up is collected
-    	// Modifies: time
-    	// Effects: changes the time when time power-up collected
-    	
-        int initTime = escapeFromKocGame.getGameState().getTime();
-        escapeFromKocGame.getGameState().setTime(initTime + 5);
+    public void checkNotBetweenCoordinatesTest() throws Exception{
 
-        assertNotEquals(escapeFromKocGame.getGameState().getTime(), initTime);
-
+    	// Requires: player wants to catch life power-up 
+    	// Modifies: doesn't modify health
+    	// Effects: health doesn't change as the power-up is not collected since it is not next to the player
+    	
+    	IPowerup lifePowerup = PowerupFactory.getInstance().createPowerup("life"); // or for any other power-up type
+    	lifePowerup.setLocation(200,100,25,25);
+    	
+    	escapeFromKocGame.setPowerupController(powerupController); 
+    	escapeFromKocGame.setPlayer(player); 
+    	powerupController.setPowerup(lifePowerup);  
+    	
+    	int initHealth = player.getPlayerState().getHealth();
+    	escapeFromKocGame.catchPowerUp(0, 10);
+    	 
+        assertNotEquals(player.getPlayerState().getHealth(), initHealth+1);
+    
     }
 
     @Test
-    public void checkCoordinatesTest(){
-       
-    	// Requires: power-ups are created
-    	// Modifies: this
-    	// Effects: each power-ups coordinates and width height are checked
+    public void catchBottlePowerupTest() throws Exception{
+
+    	// Requires: player wants to catch bottle power-up 
+    	// Modifies: power-up count in the inventory
+    	// Effects:  checks if the power-up is incremented by 1 in the inventory
     	
     	IPowerup bottlePowerup = PowerupFactory.getInstance().createPowerup("bottle");
-
-        bottlePowerup.setLocation(200,100,25,25);
-
-        assertEquals("powerup is on correct x coordinate", 200, bottlePowerup.getLocation().getXLocation(), 0.0001);
-        assertEquals("powerup is on correct y coordinate", 100, bottlePowerup.getLocation().getYLocation(),0.0001);
-
-        assertEquals("powerup has correct height", 25, bottlePowerup.getHeight());
-        assertEquals("powerup has correct width", 25, bottlePowerup.getWidth());
-        
-        
-        IPowerup timePowerup = PowerupFactory.getInstance().createPowerup("time");
-
-        timePowerup.setLocation(100,50,25,25);
-
-        assertEquals("powerup is on correct x coordinate", 100, timePowerup.getLocation().getXLocation(), 0.0001);
-        assertEquals("powerup is on correct y coordinate", 50, timePowerup.getLocation().getYLocation(),0.0001);
-
-        assertEquals("powerup has correct height", 25, timePowerup.getHeight());
-        assertEquals("powerup has correct width", 25, timePowerup.getWidth());
-        
-        
-        IPowerup lifePowerup = PowerupFactory.getInstance().createPowerup("life");
-
-        lifePowerup.setLocation(300,150,25,25);
-
-        assertEquals("powerup is on correct x coordinate", 300, lifePowerup.getLocation().getXLocation(), 0.0001);
-        assertEquals("powerup is on correct y coordinate", 150, lifePowerup.getLocation().getYLocation(),0.0001);
-
-        assertEquals("powerup has correct height", 25, lifePowerup.getHeight());
-        assertEquals("powerup has correct width", 25, lifePowerup.getWidth());
-        
-        
-        IPowerup hintPowerup = PowerupFactory.getInstance().createPowerup("hint");
-
-        hintPowerup.setLocation(200,100,25,25);
-
-        assertEquals("powerup is on correct x coordinate", 200, hintPowerup.getLocation().getXLocation(), 0.0001);
-        assertEquals("powerup is on correct y coordinate", 100, hintPowerup.getLocation().getYLocation(),0.0001);
-
-        assertEquals("powerup has correct height", 25, hintPowerup.getHeight());
-        assertEquals("powerup has correct width", 25, hintPowerup.getWidth());
-        
-        
-        IPowerup vestPowerup = PowerupFactory.getInstance().createPowerup("vest");
-
-        vestPowerup.setLocation(200,180,25,25);
-
-        assertEquals("powerup is on correct x coordinate", 200, vestPowerup.getLocation().getXLocation(), 0.0001);
-        assertEquals("powerup is on correct y coordinate", 180, vestPowerup.getLocation().getYLocation(),0.0001);
-
-        assertEquals("powerup has correct height", 25, vestPowerup.getHeight());
-        assertEquals("powerup has correct width", 25, vestPowerup.getWidth());
-
-    }
-
-    @Test
-    public void createPowerupTest(){
-
-    	// Modifies: power-up
-    	// Effects: checks if each power-up type is created correctly
+    	bottlePowerup.setLocation(200,100,25,25);
     	
-        this.bottlePowerup = PowerupFactory.getInstance().createPowerup("bottle");
-        this.timePowerup = PowerupFactory.getInstance().createPowerup("time");
-        this.vestPowerup = PowerupFactory.getInstance().createPowerup("vest");
-        this.hintPowerup = PowerupFactory.getInstance().createPowerup("hint");
-        this.lifePowerup = PowerupFactory.getInstance().createPowerup("life");
-
-        assertNotNull("Bottle powerup is created", this.bottlePowerup);
-        assertTrue("Bottle powerup is a powerup", bottlePowerup instanceof IPowerup);
-
-        assertNotNull("Time powerup is created", this.timePowerup);
-        assertTrue("Time powerup is a powerup", timePowerup instanceof IPowerup);
-
-        assertNotNull("Vest powerup is created", this.vestPowerup);
-        assertTrue("Vest powerup is a powerup", vestPowerup instanceof IPowerup);
-
-        assertNotNull("Hint powerup is created", this.hintPowerup);
-        assertTrue("Hint powerup is a powerup", hintPowerup instanceof IPowerup);
-
-        assertNotNull("Life powerup is created", this.lifePowerup);
-        assertTrue("Life powerup is a powerup", lifePowerup instanceof IPowerup);
-
-        assertNull("Nothing is not created because of invalid type", null);
-    }
-
-    @Test
-    public void checkDeletePowerupTest(){
-
-    	// Requires: power-ups are created
-    	// Modifies: created powerup's are changed to null
-    	// Effects: power-ups are deleted and they are set to null
+    	escapeFromKocGame.setPowerupController(powerupController); 
+    	escapeFromKocGame.setPlayer(player); 
+    	powerupController.setPowerup(bottlePowerup);  
     	
-        IPowerup bottlePowerup = PowerupFactory.getInstance().createPowerup("bottle");
-        powerupController.deletePowerup(bottlePowerup);
-        assertEquals("Powerup is deleted", powerupController.getPowerup(), null);
-        
-        
-        IPowerup timePowerup = PowerupFactory.getInstance().createPowerup("time");
-        powerupController.deletePowerup(timePowerup);
-        assertEquals("Powerup is deleted", powerupController.getPowerup(), null);
-        
-        
-        IPowerup lifePowerup = PowerupFactory.getInstance().createPowerup("life");
-        powerupController.deletePowerup(lifePowerup);
-        assertEquals("Powerup is deleted", powerupController.getPowerup(), null);
-        
-        
-        IPowerup vestPowerup = PowerupFactory.getInstance().createPowerup("vest");
-        powerupController.deletePowerup(vestPowerup);
-        assertEquals("Powerup is deleted", powerupController.getPowerup(), null);
-        
-        
-        IPowerup hintPowerup = PowerupFactory.getInstance().createPowerup("hint");
-        powerupController.deletePowerup(hintPowerup);
-        assertEquals("Powerup is deleted", powerupController.getPowerup(), null);
-    }
-
-       
-    @Test
-    public void checkPowerupInventoryTest() throws Exception{
-    	
-    	// Requires: power-ups are created
-    	// Modifies: power-up count in the inventory
-    	// Effects: checks if the power-up created is incremented by 1 in the inventory
-    	
-        IPowerup bottlePowerup = PowerupFactory.getInstance().createPowerup("bottle");
     	int bottlePowerupCount = player.getPlayerState().getInventory().getPowerupCount(bottlePowerup.getType());
-		player.getPlayerState().getInventory().incrementPowerups(bottlePowerup.getType());
+    	escapeFromKocGame.catchPowerUp(200, 100);
 		assertEquals(bottlePowerupCount+1, player.getPlayerState().getInventory().getPowerupCount(bottlePowerup.getType()));
-		
-		
-		IPowerup hintPowerup = PowerupFactory.getInstance().createPowerup("hint");
-	    int hintPowerupCount = player.getPlayerState().getInventory().getPowerupCount(hintPowerup.getType());
-		player.getPlayerState().getInventory().incrementPowerups(hintPowerup.getType());
-		assertEquals(hintPowerupCount+1, player.getPlayerState().getInventory().getPowerupCount(hintPowerup.getType()));
-		
-		
-		IPowerup vestPowerup = PowerupFactory.getInstance().createPowerup("vest");
+
+    }
+    
+    @Test
+    public void catchVestPowerupTest() throws Exception{
+
+    	// Requires: player wants to catch vest power-up 
+    	// Modifies: power-up count in the inventory
+    	// Effects: checks if the power-up is incremented by 1 in the inventory
+    	
+    	IPowerup vestPowerup = PowerupFactory.getInstance().createPowerup("vest");
+    	vestPowerup.setLocation(200,100,25,25);
+    	
+    	escapeFromKocGame.setPowerupController(powerupController); 
+    	escapeFromKocGame.setPlayer(player); 
+    	powerupController.setPowerup(vestPowerup);  
+    	
     	int vestPowerupCount = player.getPlayerState().getInventory().getPowerupCount(vestPowerup.getType());
-		player.getPlayerState().getInventory().incrementPowerups(vestPowerup.getType());
+    	escapeFromKocGame.catchPowerUp(200, 100);
 		assertEquals(vestPowerupCount+1, player.getPlayerState().getInventory().getPowerupCount(vestPowerup.getType()));
+
+    }
+    
+    @Test
+    public void catchHintPowerupTest() throws Exception{
+
+    	// Requires: player wants to catch hint power-up 
+    	// Modifies: power-up count in the inventory
+    	// Effects: checks if the power-up is incremented by 1 in the inventory
+    	
+    	IPowerup hintPowerup = PowerupFactory.getInstance().createPowerup("hint");
+    	hintPowerup.setLocation(200,100,25,25);
+    	
+    	escapeFromKocGame.setPowerupController(powerupController); 
+    	escapeFromKocGame.setPlayer(player); 
+    	powerupController.setPowerup(hintPowerup);  
+    	
+    	int hintPowerupCount = player.getPlayerState().getInventory().getPowerupCount(hintPowerup.getType());
+    	escapeFromKocGame.catchPowerUp(200, 100);
+		assertEquals(hintPowerupCount+1, player.getPlayerState().getInventory().getPowerupCount(hintPowerup.getType()));
+
     }
 }
