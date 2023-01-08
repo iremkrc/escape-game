af94fd2ff90c220f57dff321be6f8476623b21ad
meterdogan07 <merdogan18@ku.edu.tr>
2023-01-08 15:57:49 +0300

    Representation invariant and Abstract function added.

--- EscapeFromKoc/src/Domain/Player/Inventory.java
+++ EscapeFromKoc/src/Domain/Player/Inventory.java
@@ -1,6 +1,7 @@
 package Domain.Player;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class Inventory {
     /*
@@ -10,7 +11,17 @@ public class Inventory {
      *  		These are the hint, protection-vest and bottle power-ups
      *  		An instance of this class is in the PlayerState object.
      *  		Also, the PlayerController manipulates this class through the PlayerState.
-     */
+	 *
+	 *  Abstract Function:
+	 *  		AC(c) = {c.powerupsMap.get(powtype) | c.powerupList.contains(powtype)}
+	 *
+	 *  The Representation Invariant:
+	 *	        (c.powerupsMap not null) &&
+     *	        (c.powerupsMap.size() == c.powerupList.length) &&
+     * 	        (there are no duplicates in c.powerupList) &&
+     *			(c.powerupsMap includes all c.powerupList[0]...c.powerupList[size-1] as keys) &&
+     *			(all c.powerupsMap.get(c.powerupList[0])...c.powerupsMap.get(c.pwerupList[size-1]) are non-negative)
+	 */
 	
 	private HashMap<String, Integer> powerupsMap;
     private static final String[] powerupList = {"hint", "vest", "bottle"};
@@ -23,6 +34,10 @@ public class Inventory {
 	}
 
 	public String getPowerup(String powerupType) {
+		/*
+		 * returns the power-up type as a string if its count is 
+		 * larger than 0 in the inventory or returns null otherwise.
+		 */
 		boolean powerupFound = checkInventory(powerupType);
 		if(powerupFound){
             return powerupType;
@@ -31,6 +46,9 @@ public class Inventory {
 	}
 
     public int getPowerupCount(String powerupType) {
+		/*
+		 * returns the power-up count that exists in the inventory
+		 */
 		boolean powerupFound = checkInventory(powerupType);
 		if(powerupFound){
             return powerupsMap.get(powerupType);
@@ -39,6 +57,10 @@ public class Inventory {
 	}
 
     public boolean checkInventory(String item) {
+    	/*
+    	 * This method returns true if a power-up exists in the 
+    	 * inventory and has a count greater than or equal to 1
+    	 */
 		if(!powerupsMap.containsKey(item)){
             return false;
         }else if(powerupsMap.get(item)==0){
@@ -49,6 +71,9 @@ public class Inventory {
 	}
 
     public void incrementPowerups(String powerupType) throws Exception {
+    	/*
+    	 * This method increments the count of a power-up if it is a valid collectable power-up
+    	 */
         if(!powerupsMap.containsKey(powerupType)){
             throw new Exception("Couldn't find powerup type");
         }
@@ -60,7 +85,6 @@ public class Inventory {
     	/*
     	 * This method decrements the count of a power-up if it exists in the inventory
     	 */
-   
         if(!powerupsMap.containsKey(powerupType)){
             throw new Exception("Couldn't find powerup type.");
         }
@@ -82,10 +106,23 @@ public class Inventory {
     	 */
         if (powerupsMap == null) return false;
         if (powerupsMap.size() != powerupList.length) return false;
+        if (duplicates(powerupList)) return false;
         for (int i = 0; i < powerupList.length ; i++) {
             if (!powerupsMap.containsKey(powerupList[i])) return false;
-            if(powerupsMap.get(powerupList[i]) < 0) return false;
+            if (powerupsMap.get(powerupList[i]) < 0) return false;
         }
         return true;
     }
+    
+    boolean duplicates(final String[] zipcodelist){
+    	/*
+    	 * This method is used in the repOk function to check for the duplicates in the powerupList
+    	 */
+	    HashSet<String> lump = new HashSet<String>();
+	    for (String i : zipcodelist){
+	    	if (lump.contains(i)) return true;
+	        lump.add(i);
+	        }
+	    return false;
+	}
 }
